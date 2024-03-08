9aadc60f56a14b3c6c63316fc3784834a8b06a1a
Zhang Zeyu <zhangzeyu2001@gmail.com>
2024-03-08 23:12:25 +0000

    feat: spinner while loading

--- app/page.js
+++ app/page.js
@@ -1,30 +1,14 @@
 "use client";
 
 import Image from "next/image";
-import Feed from "@/components/Feed";
+import { Feed } from "@/components/Feed";
 import { useState, useEffect } from "react";
+import { TailSpin } from 'react-loader-spinner'
 
 export default function Home() {
-  const [tweets, setTweets] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/mastodonExplore?theme=technology")
-      .then((response) => response.json())
-      .then((data) => {
-        data = data.map((tweet) => {
-          return {
-            id: tweet.id,
-            content: tweet.content,
-            author: tweet.account.display_name,
-          };
-        });
-        setTweets(data)
-      });
-  }, []);
-
   return (
     <div>
-      <Feed tweets={tweets} />
+      <Feed />
     </div>
   );
 }
--- components/Feed.js
+++ components/Feed.js
@@ -1,9 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { TailSpin } from 'react-loader-spinner'
+
 import { Tweet } from './Tweet';
 
-export default function Feed({ tweets }) {
+const Feed = () => {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/mastodonExplore?theme=technology")
+      .then((response) => response.json())
+      .then((data) => {
+        data = data.map((tweet) => {
+          return {
+            id: tweet.id,
+            content: tweet.content,
+            author: tweet.account.display_name,
+          };
+        });
+        setTweets(data)
+      });
+  }, []);
+
   return (
     <div>
-      {tweets.map(tweet => <Tweet key={tweet.id} tweet={tweet} />)}
+      {tweets.length 
+      ? tweets.map(tweet => <Tweet key={tweet.id} tweet={tweet} />)
+      : <div className="flex justify-center items-center h-64">
+        <TailSpin
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>}
     </div>
   );
 }
+
+export { Feed }
